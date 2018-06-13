b132d61eef84da09d03b6372a81c37f33d8b39c1
jucjeff <zma67@wisc.edu>
2018-06-13 11:54:46 +0200

    Fixed login page title

--- src/pages/Registration/registration.ts
+++ src/pages/Registration/registration.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../Login/login';
 
 @Component({
   selector: 'page-registration',
@@ -9,6 +10,10 @@ export class RegistrationPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  navigateToLogin() {
+    this.navCtrl.push(LoginPage);
   }
 
 }
