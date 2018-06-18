0c580ce57bf22bf3dc6addcd2b6884bba2d63323
Bansree Patel <bansreep910@yahoo.com>
2018-06-18 17:16:01 +0200

    registration fixed

--- src/pages/Registration/registration.ts
+++ src/pages/Registration/registration.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, NavParams } from 'ionic-angular';
 import { LoginPage } from '../Login/login';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-registration',
@@ -8,7 +10,21 @@ import { LoginPage } from '../Login/login';
 })
 export class RegistrationPage {
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirmpassword: string;
+  phonenumber: string;
+  jwt: string;
+
+  constructor(public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public navParams: NavParams,
+    public http: Http,
+    private storage: Storage,
+  ) {
 
   }
 
@@ -25,4 +41,35 @@ export class RegistrationPage {
     this.navCtrl.push(LoginPage);
   }
 
+  register() {
+    this.http
+      .post("http://localhost:3000/registration", {
+        username: this.username,
+        password: this.password,
+        confirmpassword: this.confirmpassword,
+        email: this.email,
+        firstname: this.firstname,
+        lastname: this.lastname,
+        phonenumber: this.phonenumber
+      })
+      .subscribe(
+        result => {
+          let token = result.json().token;
+          this.storage.set('jwt', token);
+          this.storage.set('jwtFull', result);
+          this.navigateToLogin();
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+  matchPassword() {
+    if (this.password != this.confirmpassword) {
+      alert('Passwords dont match');
+    }
+    else{
+      this.navigateToLogin();
+    }
+  }
 }
